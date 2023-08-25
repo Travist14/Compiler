f8612faebf58f695e0e79c6b52cec1d41640c7bd
Travist14 <tkt.ma@icloud.com>
2023-08-24 19:26:39 -0500

    basic tokenizing

--- compiler.py
+++ compiler.py
@@ -3,7 +3,6 @@ import argparse
 from tokenizer import Tokenizer
 
 def main():
-    # accept a c file as an argument as well as the -t flag that tokenizes the file
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='the file to be compiled')
     parser.add_argument('-t', '--tokenize', help='tokenize the file', action='store_true')
@@ -15,11 +14,11 @@ def main():
         text = f.read()
     assert type(text) == str, "text should be a string"
     
-    # tokenize the file if the -t flag is passed
     if args.tokenize:
         tokenizer = Tokenizer(text)
-        print(tokenizer.tokens)
+        tokens = tokenizer.tokenize()
 
+        print(tokens)
 
 if __name__ == '__main__':
     main()
--- tokenizer.py
+++ tokenizer.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass
 class Tokenizer():
     def __init__(self, text):
         self.text = text
-        self.tokens = self.tokenize()
 
     def tokenize(self):
         return self.text.split()
