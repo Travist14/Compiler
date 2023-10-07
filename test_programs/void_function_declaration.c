5845181e7d1c29b399c09a569fd3a0d3887218e8
Travist14 <tkt.ma@icloud.com>
2023-10-06 22:11:08 -0500

    added void function return type

---
+++ test_programs/void_function_declaration.c
@@ -0,0 +1,19 @@
+int main() {
+    int a; 
+    a = (7 + 8) * 2 / 4 + 16;
+    int b;
+    b = 8;
+    a = a / b;
+    return a;
+}
+
+void foo(int a) {
+    int c;
+    c = 4 + 12;
+}
+
+int bar() {
+    int b;
+    b = 3 + 12;
+    return b;
+}
