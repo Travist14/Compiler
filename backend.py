f1b0bcd8316b01f30d7952069a127a6722f86090
Travist14 <tkt.ma@icloud.com>
2023-11-30 12:08:36 -0600

    updated assembly output

--- backend.py
+++ backend.py
@@ -7,7 +7,11 @@ class Line:
         self.op2 = op2
 
     def __str__(self):
-        return f"{self.label} {self.instruction} {self.op1} {self.op2}"
+        # return f"{self.label} {self.instruction} {self.op1} {self.op2}"
+        if self.label is None:
+            return f"{self.instruction} {self.op1} {self.op2}"
+        else:
+            return f"{self.label}: {self.instruction} {self.op1} {self.op2}"
 
 # function that gets the number of local variables in the ir
 def num_local_variables(ir):
@@ -35,41 +39,84 @@ def setup_postamble(ir):
     return ["pop rbp", "ret"]
 
 
+def is_register(operand):
+    if operand in ["eax", "ebx", "ecx", "edx", "esi", "edi", "ebp", "esp"]:
+        return True
+    else:
+        return False
+
+
+# def ir_to_asm(ir):
+#     asm_lines = []
+
+#     for line in ir:
+
+#         if type(line) == list:
+#             continue
+#         elif "+" in line:
+#             op1 = line.split(" ")[0]
+#             op2 = line.split(" ")[2]
+
+#             if is_register(op1):
+#                 if is_register(op2):
+#                     asm_lines.append(Line(instruction=f"add {op1}, {op2}"))
+#                 else:
+#                     asm_lines.append(Line(instruction=f"add {op1}, [rbp-{op2}]"))
+#             else:
+#                 if is_register(op2):
+#                     asm_lines.append(Line(instruction=f"add [rbp-{op1}], {op2}"))
+#                 else:
+#                     asm_lines.append(Line(instruction=f"add [rbp-{op1}], [rbp-{op2}]"))
+#         elif "*" in line:
+#             op1 = line.split(" ")[0]
+#             op2 = line.split(" ")[2]
+#             if is_register(op1):
+#                 asm_lines.append(Line(instruction=f"imul {op1}, [rbp-{op2}]"))
+#             else:
+#                 asm_lines.append(Line(instruction=f"imul [rbp-{op1}], [rbp-{op2}]"))
+#         elif "=" in line:
+#             op1 = line.split(" ")[0]
+#             op2 = line.split(" ")[2]
+#             if is_register(op2):
+#                 asm_lines.append(Line(instruction=f"mov {op1}, {op2}"))
+#             else:
+#                 asm_lines.append(Line(instruction=f"mov {op1}, [rbp-{op2}]"))
+
+#     return asm_lines
+
+
 def ir_to_asm(ir):
     asm_lines = []
 
+    size = 0
+
     for line in ir:
-        if type(line) == list:  # Skip labels for now
+        if type(line) == list:
             continue
         elif "=" in line:
             parts = line.split(" ")
             dest = parts[0]
+            size += 4
             expr = parts[2:]
 
             if "+" in expr or "-" in expr or "*" in expr or "/" in expr:
                 # Arithmetic operation
-                asm_lines.append(Line(instruction="mov", op1=expr[0], op2=dest))
-                asm_lines.append(Line(instruction="add", op1=expr[2], op2=dest))
+                asm_lines.append(Line(instruction="mov", op1=expr[0], op2=f"[rbp-{size}]"))
+                asm_lines.append(Line(instruction="add", op1=expr[2], op2=f"[rbp-{size}]"))
 
             else:
                 # Assignment of a constant
-                asm_lines.append(Line(instruction="mov", op1=expr[0], op2=dest))
+                asm_lines.append(Line(instruction="mov", op1=expr[0], op2=f"[rbp-{size}]"))
 
     return asm_lines
 
     
 def convert_to_backend(ir, symbol_table):
     
-    # print(ir)
-    # output = []
-    # output.extend(setup_preamble(ir))
-    # output.extend(ir_to_asm(ir))
-    # output.extend(setup_postamble(ir))
-    
-    # return output
-    
     output = []
+    output.extend(setup_preamble(ir))
     output.extend(ir_to_asm(ir))
+    output.extend(setup_postamble(ir))
     for line in output:
         print(line)
 
